11cd16d3e2fac15e4fcc60791310378a7c103a86
소희 <y04828@naver.com>
2026-01-23 18:36:27 +0900

    [feat] 알림푸쉬 설정  dto 생성

--- src/main/java/com/example/afternote/domain/user/dto/UserPushSettingsResponse.java
+++ src/main/java/com/example/afternote/domain/user/dto/UserPushSettingsResponse.java
@@ -1,12 +1,15 @@
 package com.example.afternote.domain.user.dto;
 
+import com.example.afternote.domain.user.model.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
-public class PushSettingResponse {
+public class UserPushSettingsResponse {
 
     @Schema(description = "타임레터 푸시 알림 수신 여부", example = "true")
     private boolean timeLetter;
@@ -16,4 +19,12 @@ public class PushSettingResponse {
 
     @Schema(description = "애프터노트 푸시 알림 수신 여부", example = "true")
     private boolean afterNote;
+
+    public static UserPushSettingsResponse from(User user) {
+        return UserPushSettingsResponse.builder()
+                .timeLetter(user.isTimeLetterPushEnabled())
+                .mindRecord(user.isMindRecordPushEnabled())
+                .afterNote(user.isAfterNotePushEnabled())
+                .build();
+    }
 }
---
+++ src/main/java/com/example/afternote/domain/user/dto/UserUpdatePushSettingsRequest.java
@@ -0,0 +1,17 @@
+package com.example.afternote.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class UserUpdatePushSettingsRequest {
+
+    @Schema(description = "타임레터 푸시 알림 수신 여부", example = "false")
+    private boolean timeLetter;
+
+    @Schema(description = "마음의 기록 푸시 알림 수신 여부", example = "true")
+    private boolean mindRecord;
+
+    @Schema(description = "애프터노트 푸시 알림 수신 여부", example = "false")
+    private boolean afterNote;
+}
