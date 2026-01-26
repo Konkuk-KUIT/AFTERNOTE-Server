b3b5baadaa0f47a125c24d41422d48aea0b06ba3
소희 <y04828@naver.com>
2026-01-27 00:10:44 +0900

    [feat] MindRecord service 구현

--- src/main/java/com/example/afternote/domain/mindrecord/dto/GetMindRecordListRequest.java
+++ src/main/java/com/example/afternote/domain/mindrecord/dto/GetMindRecordListRequest.java
@@ -1,6 +1,7 @@
 package com.example.afternote.domain.mindrecord.dto;
 
 import com.example.afternote.domain.mindrecord.model.MindRecordType;
+import com.example.afternote.domain.mindrecord.model.ViewType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GetMindRecordListRequest {
 
-    @Schema(description = "기록 유형", example = "DIARY", nullable = true)
+    @Schema(description = "기록 유형", example = "DIARY", nullable = false)
     private MindRecordType type;
 
     @Schema(description = "조회 화면 타입", example = "LIST", nullable = true)
-    private String view; // LIST, CALENDAR
+    private ViewType view; // LIST, CALENDAR
 
     @Schema(description = "연도 (캘린더 조회 시)", example = "2026", nullable = true)
     private Integer year;
@@ -21,6 +22,9 @@ public class GetMindRecordListRequest {
     @Schema(description = "월 (캘린더 조회 시)", example = "1", nullable = true)
     private Integer month;
 
-    @Schema(description = "특정 날짜 조회 (LIST 조회 시)", example = "2026-01-23", nullable = true)
-    private String date;
+
+    public boolean isCalendarView() {
+        return view == ViewType.CALENDAR;
+    }
+
 }
--- src/main/java/com/example/afternote/domain/mindrecord/repository/MindRecordRepository.java
+++ src/main/java/com/example/afternote/domain/mindrecord/repository/MindRecordRepository.java
@@ -1,6 +1,7 @@
 package com.example.afternote.domain.mindrecord.repository;
 
 import com.example.afternote.domain.mindrecord.model.MindRecord;
+import com.example.afternote.domain.mindrecord.model.MindRecordType;
 import com.example.afternote.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,12 +13,13 @@ import java.util.List;
 public interface MindRecordRepository extends JpaRepository<MindRecord, Long> {
 
     // 날짜 범위 조회 (CALENDAR)
-    List<MindRecord> findByUserAndRecordDateBetween(
+    List<MindRecord> findByUserAndTypeAndRecordDateBetween(
             User user,
+            MindRecordType type,
             LocalDate startDate,
             LocalDate endDate
     );
 
     // 날짜 조건 없는 전체 조회 (LIST 기본)
-    List<MindRecord> findByUser(User user);
+    List<MindRecord> findByUserAndType(User user, MindRecordType type);
 }
